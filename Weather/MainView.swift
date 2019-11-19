e28054c80861d2a3ea92ceb1c73f6e5a4e1a1b42
Pavel Nadolski <pavel.nadolski@targsoftware.com>
2019-11-19 10:38:05 +0300

    init

---
+++ Weather/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Pavel N on 11/18/19.
+//  Copyright © 2019 Pavel N. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
